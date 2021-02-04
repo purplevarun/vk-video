4ba98fb3dcefe2cdcb283b614dfbfeba929d3e24
purplevarun <purplevarun@gmail.com>
2021-02-04 19:09:56 +0530

    .

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const videoGrid = document.getElementById('video-grid');
-// console.log(videoGrid);
+console.log(videoGrid);
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 let myVideoStream;
