4e49f4af5973460c5fabd11b18b51dcacac6b64a
purplevarun <purplevarun@gmail.com>
2021-02-04 21:22:38 +0530

    .

--- public/script.js
+++ public/script.js
@@ -16,19 +16,30 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream =>{
     myVideoStream = stream;
     addVideoStream(myVideo,stream);
+    
+    peer.on('call',call => {
+        call.answer (stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video,userVideoStream);
+        });
+    });
+
+    socket.on('user-connected',(userId) => {
+        connectToNewUser(userId,stream);
+    });
+    
+    
 });
 peer.on("open",id => {
     socket.emit("join-room",ROOM_ID, id);    
 });
 
 
-socket.on('user-connected',(userId,) => {
-    connectToNewUser(userId,stream);
-    
-});
-const connectToNewUser = (userId) => {
+
+const connectToNewUser = (userId,stream) => {
     console.log("new user connected by varun = "+userId);
-    const call = myPeer.call(userId,stream);
+    const call = peer.call(userId,stream);
     const video = document.createElement('video');
     call.on("stream", userVideoStream => {
         addVideoStream(video, userVideoStream);
